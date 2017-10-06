65a453803ac7905447f8830918c726ae21cb43de
billbok <nicolas.boq@gmail.com>
2017-10-06 19:30:19 +0200

    test

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <hello></hello>
+
+  </div>
+</template>
+
+
+
+<script>
+import Hello from './components/Hello.vue'
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+
+
+<style>
+
+</style>
